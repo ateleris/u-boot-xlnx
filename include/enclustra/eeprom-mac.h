ae1da722cf88e9bf37a94b1d597ab98bfcd6b0a3
Filip Schramka <filip.schramka@ateleris.ch>
2024-05-14 13:51:53 +0200

    added patch Enclustra Patch 20: ds28-eeprom-fix. Prevents fuzzing during petalinux build, operates on the same files anyway.

--- include/enclustra/eeprom-mac.h
+++ include/enclustra/eeprom-mac.h
@@ -4,8 +4,12 @@
  * <info@enclustra.com>
  */
 
+#define DS28_I2C_ADDR 0x5C
+#define DS28_SYS_I2C_EEPROM_BUS 0
+
 struct eeprom_mem {
     int (*mac_reader)(u8 *buffer);
 };
 
+int ds28_get_mac(u8 *buffer);
 int atsha204_get_mac(u8 *buffer);
